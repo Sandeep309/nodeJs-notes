39b57286dafec02b7428f188c7089af67f172b26
Sandeep309 <sandeepsaini6237@gmail.com>
2024-02-01 11:01:48 +0530

    ejs

---
+++ expressApp/appViewEngine.js
@@ -0,0 +1,11 @@
+// ? View engines are useful for rendering web pages.
+// ? There are many view engines available in the market like Mustache, Handlebars, EJS, etc
+// ? but the most popular among them is EJS which simply stands for Embedded JavaScript.
+// ? It is a simple templating language / engine that lets its user generate HTML with plain javascript.
+
+
+const express = require('express');
+
+const app = express();
+
+
